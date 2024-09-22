3a943ad6426b6579c914bc063e4fad28b4b3d001
Rohith Vst <rohiththammu@gmail.com>
2024-09-22 21:10:19 +0530

    Logging and exception

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirments(file_path:str)->List[str] :
         requirements = [req.replace("\n","")for req in requirements]
         if HYPEN_E_DOT in requirements:
            requirements.remove(HYPEN_E_DOT) 
-    return requirements
+    return requirements 
  
 setup(
 name ='mlproject',
